9315d362173504fbbb333f3824184bbadb117e9e
Nicolas Yanase <142536049+Nick-Yanase@users.noreply.github.com>
2024-05-29 00:53:15 -0300

    update css and js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header"
+import Article from "./components/Article"
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Article />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Post/index.js
@@ -0,0 +1,10 @@
+const Post = ()=>{
+  return(
+
+    <>
+    <h1>posts</h1>
+    <h2>posts</h2>
+    </>
+  )
+}
+export default Post;
