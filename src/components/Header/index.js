6bceb371274c84064d3f6a31057692a92d506fef
Nicolas Yanase <142536049+Nick-Yanase@users.noreply.github.com>
2024-05-31 13:06:23 -0300

    I configured the structure for tailwind

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,25 @@
-const Header = ()=>{
+import logosvg from "../../img/logo.svg";
+const Header = () => {
+  const descLogo = "Essa é a logo do blog";
   return (
-    <h1>Header.js</h1>
-  )
-}
+    <>
+      <header className="border w-full flex justify-center ">
+        <nav className="w-full max-w-screen-lg bg-slate-400 flex justify-between items-center">
+          <div className="logo">
+            <img src={logosvg} alt={descLogo} />
+          </div>
+          <div className="search">
+            <input type="text" name="search"/>
+          </div>
+          <ul className="flex gap-3 items-center ">
+            <li><a href="#" className=" inline-block border p-2 text-regal-blue ">Option1</a></li>
+            <li><a href="#">Option1</a></li>
+            <li><a href="#">Option1</a></li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
 
 export default Header;
